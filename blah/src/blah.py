c71b009e0a05289c3168ac1768bbec7dada9e576
Omar Garcia <ogarci79@gmail.com>
2018-06-22 18:20:21 -0600

    Modified directory structure

---
+++ blah/src/blah.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import linalg 
+
+def main():
+    print("Main")
+
+if __name__ == "__main__":
+    main()
+
