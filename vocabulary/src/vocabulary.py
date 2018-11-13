269dfa7db918d6c0cf5f74a890ab3f0fe42eb1a7
Omar Garcia <ogarci79@gmail.com>
2018-11-13 06:37:48 -0700

    250 words

--- vocabulary/src/vocabulary.py
+++ vocabulary/src/vocabulary.py
@@ -15,36 +15,42 @@ def readFile(words):
 
 
 def parse():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max, help='sum the integers (default: find the max)')
+    parser = argparse.ArgumentParser(description='Get smarter')
+    parser.add_argument('words', type=int, default=0, nargs="?", help='max number of words to review, defalut "All"')
 
     args = parser.parse_args()
-    print args.accumulate(args.integers)
+    return args.words
 
 def main():
 
     words=[]
     words=readFile(words)
+    p=parse()
+    w = p if p != 0  else len(words)
+    w = w if w < len(words) else len(words)
     deck = list(range(len(words)))
+    random.shuffle(deck)
+    random.shuffle(deck)
     random.shuffle(deck)
 
     k=1
-    for n in deck:
+    for n in deck[:w]:
         os.system('clear')
-        print("\n\n\n\n\t\t\t\t\t\t\t\t" + words['Word'][n] + "\n\n\n\n\n\n\n\n\n\n\n")
+        print("\n\n\n\n\t\t\t\t\t\t\t\t" + words['Word'][n] + " (" + words['Type'][n] + ")\n\n\n\n\n\n\n\n\n\n\n")
         s=raw_input("Press Enter to continue ...")
         os.system('clear')
-        print("\n\n\n\n\t\t\t\t\t\t\t\t" + words['Word'][n] + "\n\n")
+        print("\n\n\n\n\t\t\t\t\t\t\t\t" + words['Word'][n] + " (" + words['Type'][n] + ")\n\n")
         print("\t\t\t\t\t\t\t\tDefinition: " + words['Definition'][n])
         print("\t\t\t\t\t\t\t\tSynonyms: " + words['Synonyms'][n])
         print("\t\t\t\t\t\t\t\tSentence: " + words['Sentence'][n])
         print("\t\t\t\t\t\t\t\tType: " + words['Type'][n])
         print("\t\t\t\t\t\t\t\tPronunciation: " + words['Pronunciation'][n])
-        print("\t\t\t\t\t\t\t\tCount: " + str(k) + "\n\n\n\n")
+        print("\t\t\t\t\t\t\t\tCount: " + str(k) + " of " + str(w) + "\n\n\n\n")
         k=k+1
         s=raw_input("Press Enter to continue ...")
         os.system('clear')
+    print("\n\n\n\n\t\t\t\t\t\t\t\tAll Done(" + str(w) + "/" + str(w) +")!\n\n\n\n")
+
    
 
 if __name__ == "__main__":
