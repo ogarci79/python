7f6ea445716db821817a83e2ff801b322b883842
Omar Garcia <ogarci79@gmail.com>
2018-11-18 18:58:18 -0700

    almost done

--- vocabulary/src/vocabulary.py
+++ vocabulary/src/vocabulary.py
@@ -14,54 +14,47 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-def compare(txt,e):
+def compare(txt,e,toaddr,sub):
 
     count=0
-    if e:
-        while email(txt) and count < 2:
-            count=count+1
-        return count
-
     if txt == open('words.txt','a+').read():
         return count
     else:
         outfile=open('words.txt',"w")
         outfile.write(txt)
         outfile.close()
-        while email(txt) and count < 2:
+        while email(txt,toaddr,sub) and count < 2:
             count=count+1
     
     return count
  
-def email(txt):
+def email(txt,toaddr,sub):
   
-    toaddr=["ofgarci@sandia.gov","ogarci79@gmail.com","divaldez80@gmail.com"]
 
     print "Enter Password for words.ogarci79@gmail.com"
     password=getpass.getpass()
 
-    for to in toaddr:
-        try:
-            fromaddr = "words.ogarci79@gmail.com"
-            msg = MIMEMultipart()
-            msg['From'] = fromaddr
-            msg['To'] = to
-            msg['Subject'] = "Weekly Words: Week " + str(datetime.date.today().isocalendar()[1])
+    try:
+        fromaddr = "words.ogarci79@gmail.com"
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = ", ".join(toaddr)
+        msg['Subject'] = sub
    
-            body = txt
-            msg.attach(MIMEText(body, 'plain'))
+        body = txt
+        msg.attach(MIMEText(body, 'plain'))
     
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(fromaddr, password)
-            text = msg.as_string()
-            server.sendmail(fromaddr, to, text)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, password)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+    except:
+        print "Invalid password"
+        if server:
             server.quit()
-        except:
-            print "Invalid password"
-            if server:
-                server.quit()
-            return True
+        return True
 
     return False
 
@@ -78,27 +71,33 @@ def parse():
     parser.add_argument('--favor', '-f', help='Only Favored Words', action='store_true')
     parser.add_argument('--week', '-w', help='weekly set of 10', action='store_true')
     parser.add_argument('--email', '-e', help='Send words to ogarci79@gmail.com and ofgarci@sandia.gov', action='store_true')
+    parser.add_argument('--seed', '-s', type=int, help='set seed')
 
     args = parser.parse_args()
-    return [args.words, args.favor, args.week, args.email]
+    return [args.words, args.favor, args.week, args.email, args.seed]
 
 def main():
 
+    toaddr=["ofgarci@sandia.gov","ogarci79@gmail.com","divaldez80@gmail.com"]
     words=[]
     words=readFile(words)
-    [p,f,w,e]=parse()
+    [p,f,w,e,s]=parse()
     numWords=10
+    cede=datetime.date.today().isocalendar()[1]
 
     if f:
         words=words[pd.notnull(words['Favor'])]
         words.index=range(len(words.index))
 
     if w:
-        random.seed(datetime.date.today().isocalendar()[1])
+        random.seed(cede)
         p=numWords
 
-    w = p if p != 0  else len(words)
-    w = w if w < len(words) else len(words)
+    if s:
+        random.seed(s)
+
+    p = p if p != 0  else len(words)
+    p = p if p < len(words) else len(words)
     deck = list(range(len(words)))
 
     m=numWords
@@ -107,7 +106,7 @@ def main():
 
     k=1
     txt=""
-    for n in deck[:w]:
+    for n in deck[:p]:
         os.system('clear')
         print("\n\n\n\n\t\t\t\t\t\t\t\t" + words['Word'][n] + " (" + words['Type'][n] + ")\n\n\n\n\n\n\n\n\n\n\n")
         s=raw_input("Press Enter to continue ...")
@@ -118,17 +117,25 @@ def main():
         print("\t\t\t\t\t\t\t\tSentence: " + words['Sentence'][n])
         print("\t\t\t\t\t\t\t\tType: " + words['Type'][n])
         print("\t\t\t\t\t\t\t\tPronunciation: " + words['Pronunciation'][n])
-        print("\t\t\t\t\t\t\t\tCount: " + str(k) + " of " + str(w) + "\n\n\n\n")
+        print("\t\t\t\t\t\t\t\tCount: " + str(k) + " of " + str(p) + "\n\n\n\n")
         k=k+1
         s=raw_input("Press Enter to continue ...")
         os.system('clear')
         txt=txt + words['Word'][n] + " (" + words['Type'][n] + ") - " + words['Definition'][n] + "\n"
 
-    if f and w != len(words):
-        if compare(txt,e) == 2:
+    if f and w:
+        sub = "Weekly Words: Week " + str(cede)
+        if compare(txt,e,toaddr,sub) == 2:
+            print "Failed to Send Email!"
+    elif e:
+        sub = "Words: Random"
+        count=0
+        while email(txt,toaddr,sub) and count < 2:
+            count=count+1
+        if count==2:
             print "Failed to Send Email!"
 
-    print("\n\n\n\n\t\t\t\t\t\t\t\tAll Done(" + str(w) + "/" + str(w) +")!\n\n\n\n")
+    print("\n\n\n\n\t\t\t\t\t\t\t\tAll Done(" + str(p) + "/" + str(p) +")!\n\n\n\n")
 
 if __name__ == "__main__":
     main()
